b617be7cbde73440480f816d77d303e40cf3a95b
Tania Rascia <taniarascia@gmail.com>
2018-04-28 13:09:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to Upload a File to a Server in PHP
+
+[View Tutorial](https://www.taniarascia.com/how-to-upload-files-to-a-server-with-plain-javascript-and-php)
+
+## Goals
+- Build the simplest possible form with HTML to take a retrieve files from your local computer.
+- Send the data from an HTML form to a PHP script with vanilla JavaScript.
+- Process the data in the PHP script and move the local files to an uploads/ directory on a server.
---
+++ upload.js
@@ -0,0 +1,22 @@
+const url = 'process.php';
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const files = document.querySelector('[type=file]').files;
+    const formData = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+
+        formData.append('files[]', file);
+    }
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    }).then(response => {
+        console.log(response);
+    });
+});
