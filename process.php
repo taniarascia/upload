ff1101a5d3f39cc790d8b6c2b163d3105ba0c4c6
Tania Rascia <taniarascia@gmail.com>
2018-04-28 13:23:31 -0500

    Update process.php

--- process.php
+++ process.php
@@ -4,32 +4,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_FILES['files'])) {
         $errors = [];
         $path = 'uploads/';
-		$extensions = ['jpg', 'jpeg', 'png', 'gif'];
+	$extensions = ['jpg', 'jpeg', 'png', 'gif'];
 		
         $all_files = count($_FILES['files']['tmp_name']);
 
         for ($i = 0; $i < $all_files; $i++) {  
-			$file_name = $_FILES['files']['name'][$i];
-			$file_tmp = $_FILES['files']['tmp_name'][$i];
-			$file_type = $_FILES['files']['type'][$i];
-			$file_size = $_FILES['files']['size'][$i];
-			$file_ext = strtolower(end(explode('.', $_FILES['files']['name'][$i])));
+		$file_name = $_FILES['files']['name'][$i];
+		$file_tmp = $_FILES['files']['tmp_name'][$i];
+		$file_type = $_FILES['files']['type'][$i];
+		$file_size = $_FILES['files']['size'][$i];
+		$file_ext = strtolower(end(explode('.', $_FILES['files']['name'][$i])));
 
-			$file = $path . $file_name;
+		$file = $path . $file_name;
 
-			if (!in_array($file_ext, $extensions)) {
-				$errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
-			}
-			
-			if ($file_size > 2097152) {
-				$errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
-			}
-
-			if (empty($errors)) {
-				move_uploaded_file($file_tmp, $file);
-			}
+		if (!in_array($file_ext, $extensions)) {
+			$errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
 		}
 
-		if ($errors) print_r($errors);
+		if ($file_size > 2097152) {
+			$errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
+		}
+
+		if (empty($errors)) {
+			move_uploaded_file($file_tmp, $file);
+		}
+	}
+
+	if ($errors) print_r($errors);
     }
 }
